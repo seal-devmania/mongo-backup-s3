7da83027d36e4c5af9fe6a4baf3158a80502c084
William Lima Pereira <willguitaradmfar@gmail.com>
2023-02-16 08:49:10 -0300

    feat(bkp): Adiciona arquivo latest

--- backup.sh
+++ backup.sh
@@ -15,6 +15,7 @@ FOLDER=/backup
 DUMP_OUT=dump
 
 FILE_NAME=${FILE_PREFIX}$(date -u +${DATE_FORMAT}).tar.gz
+FILE_NAME_LATEST=${FILE_PREFIX}latest.tar.gz
 
 echo "Creating backup folder..."
 
@@ -22,20 +23,22 @@ rm -fr ${FOLDER} && mkdir -p ${FOLDER} && cd ${FOLDER}
 
 echo "Starting backup..."
 
-# mongodump --host=${MONGO_HOST} --db=${MONGO_DB} --out=${DUMP_OUT}
-
 mongodump --uri "${MONGO_URI}" ${MONGO_ARGS}
 
 echo "Compressing backup..."
 
 tar -zcvf ${FILE_NAME} ${DUMP_OUT} && rm -fr ${DUMP_OUT}
 
-echo "Uploading to S3..."
+cp ${FILE_NAME} ${FILE_NAME_LATEST}
+
+echo "Uploading backup to S3..."
 
 aws s3api put-object --bucket ${S3_BUCKET} --key ${FILE_NAME} --body ${FILE_NAME} ${S3_ARGS}
+aws s3api put-object --bucket ${S3_BUCKET} --key ${FILE_NAME_LATEST} --body ${FILE_NAME_LATEST} ${S3_ARGS}
 
 echo "Removing backup file..."
 
 rm -f ${FILE_NAME}
+rm -f ${FILE_NAME_LATEST}
 
 echo "Done!"
